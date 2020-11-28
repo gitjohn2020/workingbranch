b6d1094ed4c3bb74eb6480e9ef31c6657b72aa16
Sunando <cjohn3259@gmail.com>
2020-11-29 01:01:09 +0530

    2nd commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
+/* This is the main class */
 
 @SpringBootApplication
 public class DemoApplication {
